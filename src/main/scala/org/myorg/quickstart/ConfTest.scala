10cb10a4ff5faa926c3ffd85eb0f263e97f5fa03
remstef <steremus@gmail.com>
2015-10-02 14:16:14 +0200

    changed logging fw

--- src/main/scala/org/myorg/quickstart/ConfTest.scala
+++ src/main/scala/org/myorg/quickstart/ConfTest.scala
@@ -1,6 +1,8 @@
 package org.myorg.quickstart
 
 import com.typesafe.config.{ConfigValue, ConfigFactory}
+import org.apache.hadoop.yarn.logaggregation.LogAggregationUtils
+import org.slf4j.LoggerFactory
 import scala.collection.JavaConversions._
 
 
@@ -9,13 +11,25 @@ import scala.collection.JavaConversions._
  */
 object ConfTest {
 
+  val conf = ConfigFactory.load()
+  val LOG = LoggerFactory.getLogger(ConfTest.getClass)
+
   def main(args: Array[String]) {
-    val conf = ConfigFactory.load()
+    System.setProperty("DT.question","What?")
 
     for(entry <- conf.getConfig("DT").entrySet()){
       println(entry.getKey + "\t" + entry.getValue.render() + ":" + entry.getValue.valueType())
     }
-    
+
+    LOG.trace("this is a trace message")
+    LOG.debug("this is a debug message")
+    LOG.info("this is a info message")
+    LOG.warn("this is a warn message")
+    LOG.error("this is a error message")
+
+
+
+
   }
 
 
