6831361c2f2562b9635d3cbbedecad40ae7ec2a2
Steffen Remus <sr@owl.local>
2016-01-25 16:01:43 +0100

    more notes

--- src/main/scripts/init.scala
+++ src/main/scripts/init.scala
@@ -23,9 +23,12 @@ env.setParallelism(Int.MaxValue)
 ///
 // useful commands
 ///
+
 //val ct = d.map(CtFromString[CT2red[String,String],String,String](_))
 //val ct = d.map(CtFromString[CT2def[String,String],String,String](_))
 //val ct = d.map(CtFromString[CT2ext[String,String],String,String](_))
+
+// prettyprint
 //ct.first(1).map(_.prettyPrint).print()
 
 // filter
@@ -33,3 +36,16 @@ env.setParallelism(Int.MaxValue)
 
 // sort globally by arbitrary value
 // ct.map(c => (1, c.lmi_n, c)).groupBy(0).sortGroup(1, Order.DESCENDING).map(_._3)
+
+// compute CT2
+//val ctp = ComputeCT2[CT2red[String, String], CT2ext[String, String], String, String]().process(ct)
+
+// prune
+// ctp.filter(_.n11 >= DSTaskConfig.param_min_n11).filter(_.n1dot >= DSTaskConfig.param_min_n1dot).filter(ct => ct.odot1 <= DSTaskConfig.param_max_odot1 && ct.odot1 >= DSTaskConfig.param_min_odot1)
+// ctp.map(ct => (ct, sigfun(ct))).filter(_._2 >= DSTaskConfig.param_min_sig).groupBy("_1.a").sortGroup("_2", order).first(DSTaskConfig.param_topn_sig).map(_._1)
+
+// compute DT
+//val dt = ComputeDTSimplified.byJoin[CT2red[String,String],String,String]().process(ct)
+
+// filter and sort DT
+//val dtf = FilterSortDT[CT2red[String,String],String, String](_.n11).process(dt)
