5d8ec64ba717c906a4822e4e737aa6b8e0dc1538
Steffen Remus <sr@owl.local>
2016-01-25 12:02:40 +0100

    flink shell convenience

--- src/main/scripts/prettyprint.scala
+++ src/main/scripts/prettyprint.scala
@@ -1,12 +1,17 @@
+///
+// convenience: import all flinkdt resources
+///
 import de.tudarmstadt.lt.flinkdt.textutils._
 import de.tudarmstadt.lt.flinkdt.types._
-import org.apache.flink.api.scala.ExecutionEnvironment
+import de.tudarmstadt.lt.flinkdt.tasks._
+import de.tudarmstadt.lt.flinkdt.pipes._
+import de.tudarmstadt.lt.flinkdt.examples._
 
-
-// import org.apache.flink.api.scala._
+//import org.apache.flink.api.scala.ExecutionEnvironment
+//import org.apache.flink.api.scala._
 //val env = ExecutionEnvironment.getExecutionEnvironment
-//val ds = env.readTextFile("")
+//val d = env.readTextFile("")
 
-val ct = ds.map(CtFromString[CT2def[String,String],String,String](_))
+val ct = d.map(CtFromString[CT2def[String,String],String,String](_))
 ct.map(_.prettyPrint()).print()
 
