e52207bc3844c1276ab0d672278e8a710c713e19
Steffen <sr@owl.local>
2016-03-08 16:04:40 +0100

    updated jobimtext pipeline

--- src/main/scala/de/tudarmstadt/lt/flinkdt/pipes/JoBimTextCP.scala
+++ src/main/scala/de/tudarmstadt/lt/flinkdt/pipes/JoBimTextCP.scala
@@ -16,9 +16,13 @@
 
 package de.tudarmstadt.lt.flinkdt.pipes
 
-import de.tudarmstadt.lt.flinkdt.Util
+import java.text.SimpleDateFormat
+import java.time.Duration
+import java.util.Date
+
 import de.tudarmstadt.lt.flinkdt.tasks._
-import de.tudarmstadt.lt.flinkdt.types.{CT2def, CT2red}
+import de.tudarmstadt.lt.flinkdt.textutils.CtFromString
+import de.tudarmstadt.lt.flinkdt.types.{CT2ext, CT2red}
 import org.apache.flink.api.common.operators.Order
 import org.apache.flink.api.scala._
 
@@ -29,43 +33,69 @@ object JoBimTextCP extends App {
 
   def pipeline = {
     Checkpointed(
-      Extractor(extractorfun, inputcolumn = DSTaskConfig.in_text_column) ~>
-      N11Sum[CT2red[String, String], String, String](),
-      out = DSTaskConfig.out_accumulated_AB,
+      ComputeCT2[CT2red[String, String], CT2ext[String, String], String, String](prune = true, sigfun = _.lmi_n, order = Order.ASCENDING),
+      out = DSTaskConfig.out_accumulated_CT,
       jobname = DSTaskConfig.jobname + "-1",
       reReadFromCheckpoint = true
     ) ~>
     Checkpointed(
-      ComputeCT2[CT2red[String, String], CT2def[String, String], String, String](prune = true, sigfun = _.lmi, order = Order.ASCENDING),
-      out = DSTaskConfig.out_accumulated_CT,
-      jobname = DSTaskConfig.jobname + "-2",
-      reReadFromCheckpoint = true
-    ) ~>
-    Checkpointed(
-      ComputeDTSimplified.byJoin[CT2def[String, String], String, String](),
+      ComputeDTSimplified.byJoin[CT2ext[String, String], String, String](),
       out = DSTaskConfig.out_dt,
-      jobname = DSTaskConfig.jobname + "-3",
+      jobname = DSTaskConfig.jobname + "-2",
       reReadFromCheckpoint = true
     ) ~>
     Checkpointed(
       FilterSortDT[CT2red[String, String], String, String](_.n11),
       out = DSTaskConfig.out_dt_sorted,
-      jobname = DSTaskConfig.jobname + "-4",
+      jobname = DSTaskConfig.jobname + "-3",
       reReadFromCheckpoint = true
     )
   }
 
-  var config = DSTaskConfig.resolveConfig(args)
-  if(!config.hasPath("dt.jobname"))
-    config = DSTaskConfig.resolveConfig(args ++ Array("-dt.jobname", getClass.getSimpleName.replaceAllLiterally("$","")))
-  DSTaskConfig.load(config)
+  def flipped_pipeline = {
+    Checkpointed(
+      // Begin: fliptask
+      DSTask[CT2red[String, String], CT2red[String, String]](
+        CtFromString[CT2red[String,String],String,String](_),
+        ds => { ds.map(_.flipped().asInstanceOf[CT2red[String,String]]) },
+        CtFromString[CT2red[String,String],String,String](_)) ~>
+      // End: fliptask
+      ComputeCT2[CT2red[String, String], CT2ext[String, String], String, String](prune = true, sigfun = _.lmi_n, order = Order.ASCENDING),
+      out = s"${DSTaskConfig.out_accumulated_CT}-flipped",
+      jobname = DSTaskConfig.jobname + "-1f",
+      reReadFromCheckpoint = true
+    ) ~>
+      Checkpointed(
+        ComputeDTSimplified.byJoin[CT2ext[String, String], String, String](),
+        out = s"${DSTaskConfig.out_dt}-flipped",
+        jobname = DSTaskConfig.jobname + "-2f",
+        reReadFromCheckpoint = true
+      ) ~>
+      Checkpointed(
+        FilterSortDT[CT2red[String, String], String, String](_.n11),
+        out = s"${DSTaskConfig.out_dt_sorted}-flipped",
+        jobname = DSTaskConfig.jobname + "-3f",
+        reReadFromCheckpoint = true
+      )
+  }
 
-  def extractorfun:String => TraversableOnce[CT2red[String,String]] = Util.getExtractorfunFromJobname()
 
-  // get input data
-  val in = DSTaskConfig.in_text
+  DSTaskConfig.load(DSTaskConfig.resolveConfig(args))
 
-  val ds = pipeline.process(input = DSTaskConfig.in_text)
+  val tf = new SimpleDateFormat("yyyy-MM-dd\'T\'HH:mm:ssz")
+  val start = System.currentTimeMillis()
+  var info = s"main: ${getClass.getName}\nstart: ${tf.format(new Date(start))} \nend: -- \nduration: -- "
+  DSTaskConfig.writeConfig(additional_comments = info)
+
+  var ds = pipeline.process(input = DSTaskConfig.in_text)
   ds.first(10).print
+
+  val dsf = flipped_pipeline.process(input = DSTaskConfig.in_text)
+  dsf.first(10).print
+
+  val end = System.currentTimeMillis()
+  val dur = Duration.ofMillis(end-start)
+  info = s"main: ${getClass.getName}\nstart: ${tf.format(new Date(start))} \nend: ${tf.format(new Date(end))} \nduration: ${dur.toHours} h ${dur.minusHours(dur.toHours).toMinutes} m ${dur.minusMinutes(dur.toMinutes).toMillis} ms"
+  DSTaskConfig.writeConfig(additional_comments = info, overwrite = true)
 
 }
---
+++ src/main/scala/de/tudarmstadt/lt/flinkdt/pipes/JoBimTextCPwExtractor.scala
@@ -0,0 +1,71 @@
+/*
+ *  Copyright (c) 2016
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package de.tudarmstadt.lt.flinkdt.pipes
+
+import de.tudarmstadt.lt.flinkdt.Util
+import de.tudarmstadt.lt.flinkdt.tasks._
+import de.tudarmstadt.lt.flinkdt.types.{CT2def, CT2red}
+import org.apache.flink.api.common.operators.Order
+import org.apache.flink.api.scala._
+
+/**
+  * Created by Steffen Remus
+  */
+object JoBimTextCPwExtractor extends App {
+
+  def pipeline = {
+    Checkpointed(
+      Extractor(extractorfun, inputcolumn = DSTaskConfig.in_text_column) ~>
+      N11Sum[CT2red[String, String], String, String](),
+      out = DSTaskConfig.out_accumulated_AB,
+      jobname = DSTaskConfig.jobname + "-1",
+      reReadFromCheckpoint = true
+    ) ~>
+    Checkpointed(
+      ComputeCT2[CT2red[String, String], CT2def[String, String], String, String](prune = true, sigfun = _.lmi, order = Order.ASCENDING),
+      out = DSTaskConfig.out_accumulated_CT,
+      jobname = DSTaskConfig.jobname + "-2",
+      reReadFromCheckpoint = true
+    ) ~>
+    Checkpointed(
+      ComputeDTSimplified.byJoin[CT2def[String, String], String, String](),
+      out = DSTaskConfig.out_dt,
+      jobname = DSTaskConfig.jobname + "-3",
+      reReadFromCheckpoint = true
+    ) ~>
+    Checkpointed(
+      FilterSortDT[CT2red[String, String], String, String](_.n11),
+      out = DSTaskConfig.out_dt_sorted,
+      jobname = DSTaskConfig.jobname + "-4",
+      reReadFromCheckpoint = true
+    )
+  }
+
+  var config = DSTaskConfig.resolveConfig(args)
+  if(!config.hasPath("dt.jobname"))
+    config = DSTaskConfig.resolveConfig(args ++ Array("-dt.jobname", getClass.getSimpleName.replaceAllLiterally("$","")))
+  DSTaskConfig.load(config)
+
+  def extractorfun:String => TraversableOnce[CT2red[String,String]] = Util.getExtractorfunFromJobname()
+
+  // get input data
+  val in = DSTaskConfig.in_text
+
+  val ds = pipeline.process(input = DSTaskConfig.in_text)
+  ds.first(10).print
+
+}
